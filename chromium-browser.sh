cf2482b941e6890116c27201b61117787ebb2871
Elan Ruusamäe <glen@pld-linux.org>
2009-09-24 15:34:19 +0000

    - /dev/shm check (can we do better?)
    Changed files:
        chromium-browser.sh -> 1.2

--- chromium-browser.sh
+++ chromium-browser.sh
@@ -5,10 +5,22 @@
 # found in the LICENSE file.
 
 # Always use our ffmpeg libs.
-# Also symlinkes for nss/nspr libs can be found from our dir.
-export LD_LIBRARY_PATH=/usr/lib64/chromium-browser-bin${LD_LIBRARY_PATH:+:"$LD_LIBRARY_PATH"}
+# Also symlinks for nss/nspr libs can be found from our dir.
+export LD_LIBRARY_PATH=@libdir@${LD_LIBRARY_PATH:+:"$LD_LIBRARY_PATH"}
 
 # for to find xdg-settings
-export PATH=/usr/lib64/chromium-browser-bin${PATH:+:"$PATH"}
+export PATH=@libdir@${PATH:+:"$PATH"}
 
-exec /usr/lib64/chromium-browser-bin/chromium-browser "$@"
+# chromium needs /dev/shm being mounted
+m=$(awk '$2 == "/dev/shm" && $3 == "tmpfs" {print}' /proc/mounts)
+if [ -z "$m" ]; then
+	cat >&2 <<-'EOF'
+	Chromium needs /dev/shm being mounted for Shared Memory access.
+
+	To do so, invoke (as root):
+	mount -t tmpfs -o rw,nosuid,nodev,noexec none /dev/shm
+
+	EOF
+fi
+
+exec @libdir@/chromium-browser "$@"
