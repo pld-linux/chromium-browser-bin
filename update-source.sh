f7d776fedd1b5fb7c05104b32773f1ce55a11e4e
Elan Ruusamäe <glen@pld-linux.org>
2009-11-20 14:26:29 +0000

    - cleanup; formatting
    Changed files:
        update-source.sh -> 1.3

--- update-source.sh
+++ update-source.sh
@@ -1,21 +1,13 @@
-#!/bin/sh                                                             
+#!/bin/sh
 
-if [ ""!="$1" ]
-then           
-  rev=$1       
-  fi             
+if [ "$1" ]; then
+	rev=$1
+	echo "Using $rev..."
+else
+	echo -n "Fetching latest revno... "
+	rev=$(wget -q -O - http://build.chromium.org/buildbot/snapshots/chromium-rel-linux/LATEST)
+	echo "$rev"
+fi
 
-  rev=$(wget -q -O - http://build.chromium.org/buildbot/snapshots/chromium-rel-linux/LATEST)
-
-  echo "Fetching revision $rev"
-
-  wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux/$rev/chrome-linux.zip -c -O chrome-linux32.zip
-  wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux-64/$rev/chrome-linux.zip -c -O chrome-linux64.zip
-
-#  echo -n "Update revision in spec? (yes, [N]o)? "
-#  read ans
-#  case "$ans" in
-#  [yY]) # y0 mama
-#          echo "Ok, updating."
-#;;
-#          esac
+wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux/$rev/chrome-linux.zip -c -O chrome-linux32.zip
+wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux-64/$rev/chrome-linux.zip -c -O chrome-linux64.zip
