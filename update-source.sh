f9a1143c870608cc82d03e670ec170b17676c894
Elan Ruusamäe <glen@pld-linux.org>
2009-11-20 14:30:40 +0000

    - autoup to dropin
    Changed files:
        update-source.sh -> 1.4

--- update-source.sh
+++ update-source.sh
@@ -9,5 +9,10 @@ else
 	echo "$rev"
 fi
 
-wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux/$rev/chrome-linux.zip -c -O chrome-linux32.zip
-wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux-64/$rev/chrome-linux.zip -c -O chrome-linux64.zip
+wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux/$rev/chrome-linux.zip -c -O chrome-linux32-$rev.zip
+wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux-64/$rev/chrome-linux.zip -c -O chrome-linux64-$rev.zip
+
+if [ -x dropin ]; then
+	echo "Uploading to dropin. ^C to abort"
+	./dropin chrome-linux32-$rev.zip chrome-linux64-$rev.zip
+fi
