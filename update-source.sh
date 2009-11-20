46a7b69aceb1665ecd27b9051d6e1b2bffc49e84
Sławomir Paszkiewicz <paszczus@pld-linux.org>
2009-11-20 10:08:11 +0000

    - added vip script for fetching latest snapshot for chromium-browser-bin
    Changed files:
        update-source.sh -> 1.1

---
+++ update-source.sh
@@ -0,0 +1,21 @@
+#!/bin/sh                                                             
+
+if [ ""!="$1" ]
+then           
+  rev=$1       
+  fi             
+
+  rev=$(wget -q -O - http://build.chromium.org/buildbot/snapshots/chromium-rel-linux/LATEST)
+
+  echo "Fetching revision $rev"
+
+  wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux/$rev/chrome-linux.zip -c -O chrome-linux32.zip
+  wget http://build.chromium.org/buildbot/snapshots/chromium-rel-linux-64/$rev/chrome-linux.zip -c -O chrome-linux-64.zip
+
+#  echo -n "Update revision in spec? (yes, [N]o)? "
+#  read ans
+#  case "$ans" in
+#  [yY]) # y0 mama
+#          echo "Ok, updating."
+#;;
+#          esac
